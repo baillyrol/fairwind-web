1cd1a44a418e722522b80e3636756d2330bc9bb1
Roland Bailly <roland.bailly@arhs-dev-be.com>
2021-03-25 15:51:48 +0100

    chart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { TableDataComponent } from './features/table-data/table-data.component';
+import { LineChartComponent } from './features/line-chart/line-chart.component';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'table-data', component: TableDataComponent },
+    { path: 'line-chart', component: LineChartComponent },
+
 
 ];
 
