84fa397995742c072c1af29368f96a752a4c5039
Roland Bailly <roland.bailly@arhs-dev-be.com>
2021-03-25 12:55:06 +0100

    table data

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { TableDataComponent } from './table-data/table-data.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
-    declarations: [HomeComponent, TableDataComponent],
+    declarations: [HomeComponent, TableDataComponent, DashboardComponent],
     imports: [
         CommonModule,
         SharedModule
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 
 
@@ -15,12 +18,18 @@ import { MatTableModule } from '@angular/material/table';
         MatIconModule,
         MatButtonModule,
         MatTableModule,
+        MatBadgeModule,
+        MatSidenavModule,
+        MatListModule,
     ],
     exports: [
         MatToolbarModule,
         MatIconModule,
         MatButtonModule,
-        MatTableModule
+        MatTableModule,
+        MatBadgeModule,
+        MatSidenavModule,
+        MatListModule
     ]
 })
 export class SharedModule {
