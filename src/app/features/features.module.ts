1f4c39fcabced9e48217d743b22a524ec5ef9d62
Roland Bailly <roland.bailly@arhs-dev-be.com>
2021-03-25 15:52:56 +0100

    line chart

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -4,10 +4,16 @@ import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { TableDataComponent } from './table-data/table-data.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LineChartComponent } from './line-chart/line-chart.component';
 
 
 @NgModule({
-    declarations: [HomeComponent, TableDataComponent, DashboardComponent],
+    declarations: [
+        HomeComponent,
+        TableDataComponent,
+        DashboardComponent,
+        LineChartComponent
+    ],
     imports: [
         CommonModule,
         SharedModule
